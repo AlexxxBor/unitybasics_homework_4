23a1bee94a49c3221d1bc3c9f8bf00b2abc911ed
AlexxxBor <vrar.stavropol@gmail.com>
2024-02-15 21:54:02 +0300

    Work starts

---
+++ Assets/Scripts/Controller/CharacterController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Sample
+{
+    public sealed class CharacterController : MonoBehaviour
+    {
+        private void Update()
+        {
+            //TODO:
+            //Реализовать перемещение и поворот в нужную сторону с помощью нажатия WASD или стрелочек на клавиатуре
+        }
+    }
+}
